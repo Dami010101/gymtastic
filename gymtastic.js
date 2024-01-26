567e2e48a0d971040ce13d5b8d9c551509b61d6f
Dami010101 <143275747+Dami010101@users.noreply.github.com>
2024-01-26 23:08:33 +0000

    js1

---
+++ gymtastic.js
@@ -0,0 +1,14 @@
+let img = document.getElementById('t1')
+let bold = document.getElementById('t11')
+let p = document.getElementById('t12')
+function myTraining(){
+    img.src='./asset/weight.jpg'
+    bold.innerHTML='First Training Class'
+    p.innerHTML='Lifting an appropriate amount of weight. Start with a weight you can lift comfortably 12 to 15 times,<br> instructors would be around to guide you. Learn to do each exercise correctly.'
+}
+function myTraining1(){
+    img.src='./asset/training-image-02.jpg'
+    bold.innerHTML='Second Training Class'
+    p.innerHTML='Carrying an appropriate amount of weight. Start with a weight you can lift comfortably 12 to 15 times,<br> instructors would be around to guide you. Learn to do each exercise correctly.'
+}
+
